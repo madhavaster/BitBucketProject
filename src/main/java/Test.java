0332781ac7be5a99387304407fe1b80110c00a39
MReddy <MReddy@techwave.net>
2019-09-23 23:14:29 +0530

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+/**
+ * TODO Add In Class Summary
+ * <p>
+ * TODO Add In Class Description
+ *
+ * @author 11201174 <br />
+ * Copyright 2019 Earthport Plc. All rights reserved.
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("BitBucket example");
+    }
+}
