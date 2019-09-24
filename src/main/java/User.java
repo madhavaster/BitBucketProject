3cefdbe20b13d2be7fedbf8a920f577c1e95aec9
MReddy <MReddy@techwave.net>
2019-09-24 05:53:21 +0530

    User model is added

---
+++ src/main/java/User.java
@@ -0,0 +1,33 @@
+/**
+ * TODO Add In Class Summary
+ * <p>
+ * TODO Add In Class Description
+ *
+ * @author 11201174 <br />
+ * Copyright 2019 Earthport Plc. All rights reserved.
+ */
+public class User
+{
+    private String uid;
+    private String uname;
+
+    public String getUid()
+    {
+        return uid;
+    }
+
+    public void setUid(String uid)
+    {
+        this.uid = uid;
+    }
+
+    public String getUname()
+    {
+        return uname;
+    }
+
+    public void setUname(String uname)
+    {
+        this.uname = uname;
+    }
+}
